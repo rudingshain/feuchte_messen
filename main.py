cad1ec9bd51de4cd7d250e65814a014179a0d431
Mauszentrum <jochen.leeder@mauszentrum.de>
2020-04-02 12:40:20 +0200

    Aktualisiere pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 def on_forever():
-    pass
+    pins.digital_write_pin(DigitalPin.P0, 1)
+    if pins.analog_read_pin(AnalogPin.P1) > 500:
+        basic.show_icon(IconNames.HAPPY)
+        basic.show_number(pins.analog_read_pin(AnalogPin.P1))
+    else:
+        basic.show_icon(IconNames.SAD)
+        basic.show_number(pins.analog_read_pin(AnalogPin.P1))
+    pins.digital_write_pin(DigitalPin.P0, 0)
+    basic.pause(1000)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    if (pins.analogReadPin(AnalogPin.P1) > 500) {
+        basic.showIcon(IconNames.Happy)
+        basic.showNumber(pins.analogReadPin(AnalogPin.P1))
+    } else {
+        basic.showIcon(IconNames.Sad)
+        basic.showNumber(pins.analogReadPin(AnalogPin.P1))
+    }
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.pause(1000)
 })
